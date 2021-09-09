012bf1a773234b7f2620b255ec9d575c14e378a5
Makram Kamaleddine <makramkd@users.noreply.github.com>
2021-09-09 15:27:24 -0400

    Evaluation of boolean and integer expressions

---
+++ evaluator/evaluator.go
@@ -0,0 +1,168 @@
+package evaluator
+
+import (
+	"math"
+
+	"github.com/makramkd/go-monkey/ast"
+	"github.com/makramkd/go-monkey/object"
+)
+
+var (
+	TRUE  = &object.Boolean{Value: true}
+	FALSE = &object.Boolean{Value: false}
+
+	NULL = &object.Null{}
+)
+
+func Eval(root ast.Node) object.Object {
+	switch node := root.(type) {
+	// Statements
+	case *ast.Program:
+		return evalStatements(node.Statements)
+	case *ast.ExpressionStatement:
+		return Eval(node.Expression)
+	case *ast.BlockStatement:
+		return evalStatements(node.Statements)
+
+	// Expressions
+	case *ast.IntegerLiteral:
+		return &object.Integer{Value: node.Value}
+	case *ast.BooleanLiteral:
+		return evalBool(node)
+	case *ast.PrefixExpression:
+		right := Eval(node.Right)
+		return evalPrefixExpression(node.Operator, right)
+	case *ast.InfixExpression:
+		left := Eval(node.Left)
+		right := Eval(node.Right)
+		return evalInfixExpression(node.Operator, left, right)
+	}
+
+	return nil
+}
+
+func evalStatements(statements []ast.Statement) object.Object {
+	var result object.Object
+
+	for _, stmt := range statements {
+		result = Eval(stmt)
+	}
+
+	return result
+}
+
+func evalBool(b *ast.BooleanLiteral) *object.Boolean {
+	if b.Value {
+		return TRUE
+	}
+	return FALSE
+}
+
+func nativeBoolToBoolean(b bool) *object.Boolean {
+	if b {
+		return TRUE
+	}
+	return FALSE
+}
+
+func evalInfixExpression(operator string, left, right object.Object) object.Object {
+	switch {
+	case left.Type() == object.INTEGER && right.Type() == object.INTEGER:
+		return evalIntegerInfixExpression(operator, left, right)
+	case left.Type() == object.BOOLEAN && right.Type() == object.BOOLEAN:
+		return evalBooleanInfixExpression(operator, left, right)
+	default:
+		return NULL
+	}
+}
+
+func evalBooleanInfixExpression(operator string, left, right object.Object) object.Object {
+	leftVal := left.(*object.Boolean).Value
+	rightVal := right.(*object.Boolean).Value
+
+	switch operator {
+	case "&&":
+		return nativeBoolToBoolean(leftVal && rightVal)
+	case "||":
+		return nativeBoolToBoolean(leftVal || rightVal)
+	case "==":
+		return nativeBoolToBoolean(leftVal == rightVal)
+	case "!=":
+		return nativeBoolToBoolean(leftVal != rightVal)
+	default:
+		return NULL
+	}
+}
+
+func evalIntegerInfixExpression(operator string, left, right object.Object) object.Object {
+	leftVal := left.(*object.Integer).Value
+	rightVal := right.(*object.Integer).Value
+
+	switch operator {
+	// Arithmetic operators
+	case "+":
+		return &object.Integer{Value: leftVal + rightVal}
+	case "-":
+		return &object.Integer{Value: leftVal - rightVal}
+	case "*":
+		return &object.Integer{Value: leftVal * rightVal}
+	case "/":
+		return &object.Integer{Value: leftVal / rightVal}
+	case "**":
+		return &object.Integer{Value: int64(math.Pow(float64(leftVal), float64(rightVal)))}
+	case "%":
+		return &object.Integer{Value: leftVal % rightVal}
+
+	// Comparison operators
+	case "<":
+		return nativeBoolToBoolean(leftVal < rightVal)
+	case "<=":
+		return nativeBoolToBoolean(leftVal <= rightVal)
+	case ">":
+		return nativeBoolToBoolean(leftVal > rightVal)
+	case ">=":
+		return nativeBoolToBoolean(leftVal >= rightVal)
+	case "==":
+		return nativeBoolToBoolean(leftVal == rightVal)
+	case "!=":
+		return nativeBoolToBoolean(leftVal != rightVal)
+
+	default:
+		return NULL
+	}
+}
+
+func evalPrefixExpression(operator string, right object.Object) object.Object {
+	switch operator {
+	case "!":
+		return evalBangOperator(right)
+	case "-":
+		return evalNegativeOperator(right)
+	default:
+		return NULL
+	}
+}
+
+func evalBangOperator(right object.Object) *object.Boolean {
+	switch right {
+	case TRUE:
+		return FALSE
+	case FALSE:
+		return TRUE
+	case NULL:
+		return TRUE
+	default:
+		return FALSE
+	}
+}
+
+func evalNegativeOperator(right object.Object) object.Object {
+	switch e := right.(type) {
+	case *object.Integer:
+		return &object.Integer{
+			Value: e.Value * -1,
+		}
+	default:
+		return NULL
+	}
+}
---
+++ evaluator/evaluator_test.go
@@ -0,0 +1,91 @@
+package evaluator_test
+
+import (
+	"testing"
+
+	"github.com/makramkd/go-monkey/evaluator"
+	"github.com/makramkd/go-monkey/lexer"
+	"github.com/makramkd/go-monkey/object"
+	"github.com/makramkd/go-monkey/parser"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestEvalIntegerLiteral(t *testing.T) {
+	testCases := []struct {
+		input    string
+		expected int64
+	}{
+		{"5", 5},
+		{"10", 10},
+		{"-5", -5},
+		{"-10", -10},
+		{"5 + 4", 9},
+		{"5 + 4 * 10", 45},
+		{"28 / 2 + 3 * 4 + 1", 27},
+		{"(4 + 10) * 2 + (3 + 10) * 2 + 1", 55},
+	}
+
+	for _, testCase := range testCases {
+		l := lexer.New(testCase.input)
+		p := parser.New(l)
+		program := p.ParseProgram()
+		val := evaluator.Eval(program)
+		assert.IsType(t, &object.Integer{}, val)
+		integerValue := val.(*object.Integer)
+		assert.Equal(t, testCase.expected, integerValue.Value)
+	}
+}
+
+func TestEvalBooleanLiteral(t *testing.T) {
+	testCases := []struct {
+		input    string
+		expected bool
+	}{
+		{"true", true},
+		{"false", false},
+		{"1 != 2", true},
+		{"1 == 1", true},
+		{"1 == 2", false},
+		{"1 > 1", false},
+		{"1 >= 1", true},
+		{"2 > 1", true},
+		{"2 < 1", false},
+		{"2 <= 1", false},
+		{"1 <= 2", true},
+		{"1 > 2 && 2 > 1", false},
+	}
+
+	for _, testCase := range testCases {
+		l := lexer.New(testCase.input)
+		p := parser.New(l)
+		program := p.ParseProgram()
+		val := evaluator.Eval(program)
+		assert.IsType(t, &object.Boolean{}, val)
+		boolValue := val.(*object.Boolean)
+		assert.Equal(t, testCase.expected, boolValue.Value)
+	}
+}
+
+func TestBangOperator(t *testing.T) {
+	testCases := []struct {
+		input    string
+		expected bool
+	}{
+		{"!true", false},
+		{"!false", true},
+		{"!5", false},
+		{"!!true", true},
+		{"!!false", false},
+		{"!!5", true},
+	}
+
+	for _, testCase := range testCases {
+		l := lexer.New(testCase.input)
+		p := parser.New(l)
+		program := p.ParseProgram()
+		val := evaluator.Eval(program)
+		assert.IsType(t, &object.Boolean{}, val)
+		boolValue := val.(*object.Boolean)
+		assert.Equal(t, testCase.expected, boolValue.Value)
+	}
+}
